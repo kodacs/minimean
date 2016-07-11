321777330b38bed148c130388ed2127838322847
kodacs <kodacs-ghpublic@nym.hush.com>
2016-07-11 16:27:59 +0200

    auth done

--- server/api/checkauth_mw.js
+++ server/api/checkauth_mw.js
@@ -1,9 +1,13 @@
+var express = require('express')
+var jwt = require('jsonwebtoken')
+var signingkey = process.env.npm_package_config_secretkey
+
 module.exports = function(req, res, next) {
   var token = req.body.token  ||
               req.query.token ||
               req.headers['x-access-token']
   if (token) {
-    jwt.verify(token, app.get('signingkey'), function(err, decoded) {
+    jwt.verify(token, signingkey, function(err, decoded) {
       if (err) {
         return res.json({ success: false, message: 'Failed to authenticate token.' })
       } else {
--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -1,14 +1,15 @@
-var express   = require('express')
-var mongoose  = require('mongoose')
-var User      = mongoose.model('User')
-var router    = express.Router()
+var express = require('express')
+var mongoose = require('mongoose')
+var User = mongoose.model('User')
+var router = express.Router()
 var checkAuth = require('../checkauth_mw')
 
+router.use(checkAuth)
 router.route('/')
-  .get(function(req, res) {
-      res.json({
-        "message": "get"
-      })
+  .get(function (req, res) {
+    res.json({
+      "message": "get"
+    })
   })
 
 module.exports = router

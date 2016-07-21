33f415bcdb6ad8159d1be5d58ab072605d1b3f62
kodacs <kodacs-ghpublic@nym.hush.com>
2016-07-21 13:44:16 +0200

    MATERIALIZED!!

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button/button';
 
 @Component({
   moduleId: module.id,
   selector: 'home',
-  template: '{{title}}'
+  directives: [MD_BUTTON_DIRECTIVES],
+  template: `{{title}}
+  <button md-raised-button color="primary">this is a magical button</button>
+  `
 })
 export class HomeComponent {
   title = 'home works!';
--- src/system-config.ts
+++ src/system-config.ts
@@ -7,11 +7,21 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  '@angular2-material': 'vendor/@angular2-material',
 };
 
 /** User packages configuration. */
 const packages: any = {
 };
+
+const materialPkgs:string[] = [
+  'core',
+  'button'
+];
+
+materialPkgs.forEach((pkg) => {
+  packages[`@angular2-material/${pkg}`] = {main: `${pkg}.js`};
+});
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************

9693a1d8787e831487804aad406c9a71384af29c
kodacs <kodacs-ghpublic@nym.hush.com>
2016-07-28 08:11:38 +0200

    die trying

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-// import { Http, Response, Headers } from '@angular/http';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button/button';
 import { AuthService } from './auth.service';
+import { FormGroup, FormControl, REACTIVE_FORM_DIRECTIVES, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   selector: 'login-component',
-  directives: [MD_BUTTON_DIRECTIVES],
+  directives: [MD_BUTTON_DIRECTIVES, REACTIVE_FORM_DIRECTIVES],
   providers: [AuthService],
   template: `{{title}}
   <button md-raised-button color="primary">this is a magical button</button>
@@ -17,13 +17,50 @@ import { AuthService } from './auth.service';
 <button type="button" (click)="authSrv.logOut()">Logout</button>
 <div *ngIf="loading">loading...</div>
 <pre>{{authSrv.data | json}}</pre>
-  `
+<form [formGroup]="registerForm">
+  <label>Firstname:</label>
+  <input type="text" formControlName="firstname">
+
+  <label>Lastname:</label>
+  <input type="text" formControlName="lastname">
+
+  <label>Street:</label>
+  <input type="text" formControlName="street">
+
+  <label>Zip:</label>
+  <input type="text" formControlName="zip">
+
+  <label>City:</label>
+  <input type="text" formControlName="city">
+
+  <button type="submit">Submit</button>
+</form>`
+
+// <input type="text" [formControl]="name" />`
 })
 
-
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   title = 'login works!';
 
-  constructor(private authSrv: AuthService) {
+ngOnInit() {
+  let registerForm = this.formBuilder.group({
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    address: this.formBuilder.group({
+      street: [],
+      zip: [],
+      city: []
+    })
+  });
+}
+
+
+//  let loginForm = new FormGroup ({
+//  loginUser: new FormControl(''),
+//  passwordUser: new FormControl('')
+//  });
+
+  constructor(private authSrv: AuthService, private formBuilder: FormBuilder) {
   }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import { MdIconRegistry } from '@angular2-material/icon/icon-registry';
 import { AuthService } from './app';
 import { AuthGuard } from './app';
+//import { provideForms } from '@angular/forms';
 
 
 if (environment.production) {
@@ -25,6 +26,7 @@ bootstrap(RouterComponent, [
   provide(LocationStrategy, { useClass: HashLocationStrategy }),
   AuthGuard,
   AuthService,
+//  provideForms,
   HTTP_PROVIDERS,
   MdIconRegistry
 ]);
